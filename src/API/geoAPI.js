84609a78f7a98a0b19a4acbcf726471891096965
davisstacked <brettzvi@gmail.com>
2021-05-19 16:09:38 -0700

    Flag Map API call created

---
+++ src/API/geoAPI.js
@@ -0,0 +1,20 @@
+import axios from "axios";
+const BASE_URL =
+  "https://restcountries.eu/rest/v2/all?fields=name;capital;flag;latlng";
+
+
+
+class geoApi {
+  static async getData() {
+    try {
+      const res = await axios.get(BASE_URL);
+      return res.data;
+    }
+    catch (err) {
+      console.log(err);
+    }
+  }
+};
+
+export default geoApi;
+  
