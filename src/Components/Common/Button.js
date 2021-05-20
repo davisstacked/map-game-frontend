312afb5240a231bb6a053c737d852c1531781ed1
davisstacked <brettzvi@gmail.com>
2021-05-19 18:11:06 -0700

    GameContextProvider

---
+++ src/Components/Common/Button.js
@@ -0,0 +1,14 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Button = ({ text, gradient }) => {
+  return (
+    <Link to={text} className="self-center">
+      <button className={`bg-gradient-${gradient}`}>
+        {text.charAt(0).toUpperCase() + text.slice(1)}
+      </button>
+    </Link>
+  );
+};
+
+export default Button;
