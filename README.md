72446d51adde340200da64596d8d30008e73f0c5
davisstacked <brettzvi@gmail.com>
2021-05-19 20:25:59 -0700

    helper.js with makeUniqueSelection function

--- README.md
+++ README.md
@@ -6,3 +6,5 @@ Conflicts:
 What You'd Do Differently:
 
 Be Able to Discuss Technical Components in Depth
+
+A challenge I faced was the same flag showing up potentially more than once for an option. To solve this problem, I created (in helper.js) the function hasDuplicates which returns the number. Make unique selection function.
---
+++ src/Components/Game/Common/helpers.js
@@ -0,0 +1,42 @@
+// makeUniqueSelection,
+// pickWinner,
+// isCorrect,
+// gameLost,
+// nextLevel,
+
+const COUNTRIES_LEN = 249;
+const SELECTION_LEN = 4;
+const NUM_OF_TURNS = 10;
+const NUM_OF_LEVELS = 3;
+const MAX_ACCURACY_BONUS = 250;
+const CORRECT_GUESS_SCORE = 100;
+const SPEED_BONUS = 50;
+
+export const MAX_SCORE =
+  (NUM_OF_LEVELS - 1) * (NUM_OF_TURNS * (CORRECT_GUESS_SCORE)) + (NUM_OF_TURNS * (CORRECT_GUESS_SCORE + SPEED_BONUS)) + NUM_OF_TURNS * CORRECT_GUESS_SCORE + MAX_ACCURACY_BONUS;
+
+const getRandInt = (len) => {
+  return Math.floor(Math.random() * len);
+};
+
+const hasDuplicates = (arr) => {
+  return new Set(arr).size !== arr.length;
+};
+
+export const getPercentile = (score) => {
+  return (score / MAX_SCORE) * 100;
+};
+
+export const makeUniqueSelection = () => {
+  let finalSelection = [];
+  let selectionIndeces = [];
+  while (selectionIndeces < 4 || hasDuplicates(selectionIndeces)) {
+    for (let i = 0; i < 4; i++) {
+      const num = getRandInt(COUNTRIES_LEN);
+      selectionIndeces.push(num);
+    }
+  }
+  finalSelection = selectionIndeces;
+  return finalSelection;
+}
+
