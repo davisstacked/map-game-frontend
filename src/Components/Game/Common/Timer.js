79f05c044b2f73f4c5d20d859faee927b4df1b0c
davisstacked <brettzvi@gmail.com>
2021-05-25 14:04:01 -0700

    useEffect for timer width (decreases with less time) in Timer.js

--- src/Components/Game/Common/Timer.js
+++ src/Components/Game/Common/Timer.js
@@ -7,4 +7,12 @@ const Timer = ({ setTurn, timderId }) => {
   const { setWidth, width, setLives, lives, score } = useContext(GameContext);
 
   // initiate interval to decrease width with time
+  useEffect(() => {
+    clearInterval(timerId.current);
+    timerId.current = setInterval(() => {
+      setWidth((w) => {
+        return w - 1;
+      });
+    }, 50);
+  }, [setWidth, timerId]);
 };
