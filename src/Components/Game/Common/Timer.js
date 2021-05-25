a0763651d58b4ab6fbd583adcd234eb4f656b23e
davisstacked <brettzvi@gmail.com>
2021-05-25 14:36:37 -0700

    timer.js

--- src/Components/Game/Common/Timer.js
+++ src/Components/Game/Common/Timer.js
@@ -7,14 +7,14 @@ const Timer = ({ setTurn, timerId }) => {
   const { setWidth, width, setLives, lives, score } = useContext(GameContext);
 
   // initiate interval to decrease width with time
-  useEffect(() => {
-    clearInterval(timerId.current);
-    timerId.current = setInterval(() => {
-      setWidth((w) => {
-        return w - 1;
-      });
-    }, 50);
-  }, [setWidth, timerId]);
+  // useEffect(() => {
+  //   clearInterval(timerId.current);
+  //   timerId.current = setInterval(() => {
+  //     setWidth((w) => {
+  //       return w - 1;
+  //     });
+  //   }, 50);
+  // }, [setWidth, timerId]);
 
   // handle timeOut
   useEffect(() => {
@@ -26,5 +26,31 @@ const Timer = ({ setTurn, timerId }) => {
     }
   }, [width, setLives, setTurn, setWidth]);
 
-  
+  return (
+    <>
+      <div className="w-40 my-6 border-2 border-cerise-500 rounded-xl bg-gradient-pale p-2">
+        <div className="text-center">
+          Score: <span className="text-lx font-bold">{score}</span>
+        </div>
+        <div className="text-center">
+          {Array.from({ length: lives }).map((m) => (
+            <HeartStraight
+              key={uuid()}
+              className="inline text-cerise-500 my-1"
+              size={24}
+            />
+          ))}
+        </div>
+      </div>
+      <div className="self-stretch col-start-1 col-span-8 border-2 border-black">
+        <div
+          style={{ width: `${width}%` }}
+          className="bg-gradient-purple timer"
+        >
+        </div>
+      </div>
+    </>
+  )
 };
+
+export default Timer;
