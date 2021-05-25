dc93a64a0e39d3e5749cb3f5503bd7e2bda107b0
davisstacked <brettzvi@gmail.com>
2021-05-25 13:55:28 -0700

    added imports in timer

---
+++ src/Components/Game/Common/Timer.js
@@ -0,0 +1,10 @@
+import React, { useContext, useEffect } from "react";
+import { HeartStraight } from "phosphor-react";
+import GameContext from "../../../Context/GameContext";
+import { v4 as uuid } from "uuid";
+
+const Timer = ({ setTurn, timderId }) => {
+  const { setWidth, width, setLives, lives, score } = useContext(GameContext);
+
+  // initiate interval to decrease width with time
+};
